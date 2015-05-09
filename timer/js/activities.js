d0532c5b71892943adb32f277f92f91695f3607d
Irene W <i.lalinw@gmail.com>
2015-05-09 06:57:02 -0700

    added images to activityPool (in img)

--- timer/js/activities.js
+++ timer/js/activities.js
@@ -3,7 +3,7 @@ var activityPool = [
          id: 1,
          name: 'Squats',
          description: 'Get up and do some squats!',
-         image: '',
+         image: 'img/squats.jpg',
          category: 'exercise',
          funFact: 'Strong legs helps you stay mobile as you get older. Squatting stabilizes muscles and help improve balance.',
          badge: 'active'
@@ -12,7 +12,7 @@ var activityPool = [
         id: 2,
         name: 'Stand up and walk around',
         description: 'Stand up and walk around your room, or better yet, go take a short walk outside!',
-        image: '',
+        image: 'img/walk.jpg',
         category: 'exercise',
         funFact: 'Sitting for long periods of time tends to cause changes physiologically within your muscles. Your body stops breaking up fat in your bloodstream, and you start getting accumulations of fat... in your liver, heart, brain...everywhere!',
         badge: 'active'
@@ -21,81 +21,81 @@ var activityPool = [
         id: 3,
         name: 'Jumping jacks',
         description: 'You are in a jumping jack olympics. Show everyone your best 3 sets of jumping jacks. Take break as you need it.',
-        image: '',
+        image: 'img/jumping_jacks.jpg',
         category: 'exercise',
         funFact: 'Simple but effective. Jumping jacks helps improve strength, endurance and metabolism',
         badge: 'active'
     },
     {
-        id: 3,
+        id: 4,
         name: 'Push-ups',
         description: 'Clear some space and do some push-ups. Remember to count! so you can do one more next time',
-        image: '',
+        image: 'img/push_ups.jpg',
         category: 'exercise',
         funFact: 'Performing weight-bearing exercises like pushups will strengthen your wrists, forearms, elbows, upper arms and shoulders. They will also help reduce bone loss and promote stronger, more dense bones.',
         badge: 'active'
     },
     {
-        id: 4,
+        id: 5,
         name: 'Stretch',
         description: 'Stand up and stretch. Stretch your arms, legs, neck, back and wrists.',
-        image: '',
+        image: 'img/wrist_stretch.gif',
         category: 'exercise',
         funFact: 'Repetitive Strain Injury (RSI) is very common nowadays for office workers. Stretching can keep you away from RSIs.',
         badge: 'active'
     },
     {
-        id: 5,
+        id: 6,
         name: 'Water break',
         description: 'Did you forget to drink water this work streak? Take this time to go get yourself some water or get your water bottle filled',
-        image: '',
+        image: 'img/drinking_water.jpg',
         category: 'health',
         funFact: 'When you feel thirsty, you are already very dehydrated. Make sure you keep your body hydrated and your lips uncracked!.',
         badge: 'healthy'
     },
     {
-        id: 6,
+        id: 7,
         name: 'Bathroom Break',
         description: 'Take a bathroom break! Look away from the monitor and enjoy some real life moments',
-        image: '',
+        image: 'img/bathroom_break.jpg',
         category: 'health',
         funFact: 'Bathroom breaks = Healthier bladder',
         badge: 'healthy',
     },
     {
-        id: 7,
+        id: 8,
         name: 'Declutter your workspace',
         description: 'How is your work area look like now? Stand up and organize your things. Throw away those papers and post-its pile you are done with.',
-        image: '',
+        image: 'img/declutter,png',
         category: 'chores',
         funFact: 'Getting rid of clutter lessens your external input to increase your capacity for productive work.',
         badge: 'housemaster'
     },
     {
-        id: 8,
+        id: 9,
         name: 'Food break',
         description: 'Get up and grab some food or snacks! or better yet, take a break from your work and make yourself some homemade food',
-        image: '',
+        image: 'img/lunch.jpg',
         category: 'health',
-        funFact: '',
+        funFact: 'Your body burns calories even if you are doing nothing. Muscles burn more energy than fat when you are resting.',
         badge: 'healthy'
     },
     {
-        id: 9,
+        id: 10,
         name: 'Dance break',
         description: 'Turn on the radio or music of your choice and dance away. Sit down after you have finished one song.',
-        image: '',
+        image: 'img/dance.jpg',
         category: 'exercise',
-        funFact: '',
+        funFact: 'Dancing is a good way to exercise while having fun.',
         badge: 'active'
     },
     {
-        id: 10,
+        id: 11,
         name: 'Take out the trash',
         description: 'Take out the trash, or if you don not have the time, put away the trash sitting on your desk and throw it away!.',
         image: 'img/take_out_trash.gif',
         category: 'health',
-        funFact: '',
+        funFact: 'Decluttering your space declutters your mind too.',
         badge: 'housemaster'
     }
     ];
